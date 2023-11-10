85db83932b87e487deb02b9c9ad0d478e7cb42d6
NagarjunaPavuluri <125419258+NagarjunaPavuluri@users.noreply.github.com>
2023-11-09 23:27:26 -0600

    Update delete_property.py

--- backend property/delete_property.py
+++ backend property/delete_property.py
@@ -8,14 +8,11 @@ def lambda_handler(event, context):
     # Extract the 'pid' parameter from the query string parameters
     pid = event["queryStringParameters"]["pid"]
 
-    # Delete the item from the DynamoDB table
     dydbTable.delete_item(
         Key={
             "pid": pid
         }
     )
-    
-    # Create the response body
     body = {"result": "The property is deleted"}
    
     return {
